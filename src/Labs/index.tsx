9a37113f6c6db496071cde713db63e80855e4ef9
Ghanashyam Vagale <73483236+GhanashyamVagale@users.noreply.github.com>
2024-11-06 01:34:02 -0500

    Add files via upload

---
+++ src/Labs/index.tsx
@@ -0,0 +1,32 @@
+import Lab1 from "./Lab1";
+import { Route, Routes, Navigate } from "react-router";
+import Lab2 from "./Lab2";
+import Lab3 from "./Lab3";
+import TOC from "./TOC";
+import Lab4 from "./Lab4";
+import { Provider } from "react-redux";
+import store from "./Store";
+export default function Labs() {
+  return (
+    <Provider store={store}>
+      <div className="container-fluid">
+      <h1>Ghanashyam Vagale Srinivas</h1>
+      <h1>Section 03</h1>
+      <p>
+        Link to my <a id="wd-github" href="https://github.com/GhanashyamVagale/kanbas-react-web-app">
+          <strong>GitHub</strong>
+        </a> repository.
+      </p>
+      <hr />
+        <TOC />
+        <Routes>
+          <Route path="/" element={<Navigate to="Lab1" />} />
+          <Route path="Lab1" element={<Lab1 />} />
+          <Route path="Lab2" element={<Lab2 />} />
+          <Route path="Lab3/*" element={<Lab3 />} />
+          <Route path="Lab4" element={<Lab4 />} />
+        </Routes>
+      </div>
+    </Provider>
+  );
+}
