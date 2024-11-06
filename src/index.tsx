93fd5823707c252b3e2cf8e0a742ffaec84a575c
Ghanashyam Vagale <73483236+GhanashyamVagale@users.noreply.github.com>
2024-11-06 01:14:31 -0500

    Add files via upload

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-<<<<<<< HEAD
-// import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-=======
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
-// import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
->>>>>>> acc68c40b3ca5e785466cdbed79e85c75074fb3a
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
