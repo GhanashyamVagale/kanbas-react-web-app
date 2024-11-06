038d291073d50e39c926ef5bfcc4617ebfbcbc9d
Ghanashyam Vagale <73483236+GhanashyamVagale@users.noreply.github.com>
2024-11-06 01:28:42 -0500

    Delete src directory

--- src/Kanbas/Courses/Modules/WeekGreenCheckmark.tsx
+++
@@ -1,7 +0,0 @@
-import GreenCheckmark from "./GreenCheckmark";
-export default function WeekGreenCheckmark() {
-  return (
-    <div className="float-end">
-      <GreenCheckmark />
-    </div>
-);}
